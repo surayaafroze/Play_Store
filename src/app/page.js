f36916b7b39322c06f31ec19b87417dbb4e7fe89
surayaafroze63 <surayaafroze63@gmail.com>
2026-04-12 22:09:24 +0600

    Fetching data

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import React from 'react';
 
-export default function Home() {
+const HomePage =async () => {
+  const res =await fetch('https://jsonplaceholder.typicode.com/users');
+  const data=await res.json()
+  console.log(data)
   return (
     <div>
-    
+      
     </div>
   );
-}
+};
+
+export default HomePage;
