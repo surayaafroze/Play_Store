25d3ff9945620f2bcceac24513c76cf3541c0382
surayaafroze63 <surayaafroze63@gmail.com>
2026-04-12 22:01:07 +0600

    Added navbar

---
+++ src/app/apps/page.jsx
@@ -0,0 +1,11 @@
+
+
+const AllApps = () => {
+  return (
+    <div>
+     All apps are here 
+    </div>
+  );
+};
+
+export default AllApps;
---
+++ src/app/installation/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Installations = () => {
+  return (
+    <div>
+      This is a installation page
+    </div>
+  );
+};
+
+export default Installations;
